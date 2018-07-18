db096099da2bd7d2d0303ea0e6ce15c1948a6e60
Justin <jokerbatman59@gmail.com>
2018-07-18 02:53:19 -0700

    Added genisis block

--- epiccoin-block.py
+++ epiccoin-block.py
@@ -1,17 +1,34 @@
-import hashlib as hasher
+import hashlib 
+import json
+from time import time
 
 class Block:
   def __init__(self, index, timestamp, data, previous_hash):
-    self.index = index
-    self.timestamp = timestamp
-    self.data = data
-    self.previous_hash
-    self.hash = self.has_block()
+    self.transactions = []
+    self.chain = []
 
-def hash_block(self):
-  sha = hasher.sha256()
-  sha.update(str(self.index) +
-              str(self.timestamp) +
-              str(self.data) +
-              str(self.previous_hash))
-  return sha.hexdigest()
+#create genisis block
+self.newblock(self, proof, previous_hash)
+
+#create a new block to add to blockchain
+def new_block(self, proof, previous_hash=None):
+  block = {
+  'index': len(self.chain) + 1,
+  'timestamp': time(),
+  'transactions': self.current_transactions,
+  'proof': proof,
+  'previous_hash': previous_hash or self.hash(self.chain[-1]),
+  }
+ self.current_transactions = []
+ 
+ self.chain.append(block)
+ return block
+
+def new_transaction(self,sender,rescipenet, amount)
+#creates new transaction to add to next mined block
+self.current_transaction.append({
+  'sender': sender,
+  'recipient': recipient,
+  'amount': amount,
+  })
+return self.last_block['index'] + 1
